0be9674c3a9cb181db7f7f3b30e07e32e28e9585
Graham Grochowski <ggrochow@gmail.com>
2015-12-14 17:21:46 -0800

    inital Commit'

---
+++ index.js
@@ -0,0 +1,33 @@
+var Hapi      = require('hapi');
+var Joi       = require('joi');
+var Boom      = require('boom');
+
+var PORT      = process.env.PORT || 8000;
+var server    = new Hapi.Server();
+var db        = require('./lib/models/index.js')
+
+var plugins = [
+  require('inert'),
+];
+
+server.register(plugins, function() {
+  server.connection({ port: PORT });
+
+  // Serve up all static content in public folder
+  server.route({
+    method: 'GET',
+    path: '/{p*}',
+    handler: {
+      directory: {
+        path: './public',
+        listing: false,
+        index: true,
+      }
+    }
+  });
+  
+  // Start your Server
+  server.start(function () {
+    console.log('Kindred is running on port:', PORT);
+  });
+});
