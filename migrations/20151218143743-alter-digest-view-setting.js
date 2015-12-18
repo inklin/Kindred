bb2453085a065d3efff0f8020539530c70b4ce59
Stacey Lin <twstacey@gmail.com>
2015-12-18 14:41:09 -0800

    Add not-null constraints to DigestViewSettings

---
+++ migrations/20151218143743-alter-digest-view-setting.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return [
+      queryInterface.changeColumn(
+        'DigestViewSettings',
+        'AccountId',
+        {
+          type: Sequelize.INTEGER,
+          allowNull: false
+        }
+      ),
+      queryInterface.changeColumn(
+        'DigestViewSettings',
+        'PersonId',
+        {
+          type: Sequelize.INTEGER,
+          allowNull: false
+        }
+      ),
+      queryInterface.changeColumn(
+        'DigestViewSettings',
+        'sendAs',
+        {
+          type: Sequelize.STRING,
+          allowNull: false
+        }
+      )
+    ];
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return [
+      queryInterface.changeColumn(
+        'DigestViewSettings',
+        'AccountId',
+        {
+          type: Sequelize.INTEGER,
+        }
+      ),
+      queryInterface.changeColumn(
+        'DigestViewSettings',
+        'PersonId',
+        {
+          type: Sequelize.INTEGER,
+        }
+      ),
+      queryInterface.changeColumn(
+        'DigestViewSettings',
+        'sendAs',
+        {
+          type: Sequelize.STRING,
+        }
+      )
+    ];
+  }
+};
