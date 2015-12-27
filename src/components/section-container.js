dde897047e1f140cb5c576a236d1be98be9eca8a
Graham Grochowski <ggrochow@gmail.com>
2015-12-27 12:55:01 -0800

    added fullsection component that renders under section/id view

--- src/components/section-container.js
+++ src/components/section-container.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import Section from './digest/section'
+import FullSection from './full-section'
 
 export default class SectionContainer extends React.Component {
   render() {
     return (
-      <Section 
+      <FullSection 
         imageUrl='https://upload.wikimedia.org/wikipedia/commons/2/2b/Vancouver_-_Gastown_01.jpg'
         title='Section Title'
-        body='Curabitur posuere lacinia tortor non facilisis. Praesent varius, mi ut suscipit molestie, tellus risus cursus libero, in tincidunt nisl dolor ac mauris. Praesent molestie convallis tortor, mollis tristique arcu fermentum interdum. Phasellus eleifend fringilla ullamcorper. Donec tristique, dui at blandit pretium, ex sem egestas dolor, vel fermentum magna dui at ligula. Nam erat neque, maximus et nibh eu, vehicula vestibulum lectus. Aenean non nunc sed enim pellentesque interdum. In venenatis elit nulla. Donec vulputate purus vel tortor feugiat, eu finibus odio tempus. Aliquam feugiat lacus at dolor aliquet congue.'
+        body='Curabitur posuere lacinia tortor non facilisis. Praesent varius, mi ut suscipit molestie, tellus risus cursus libero, in tincidunt nisl dolor ac mauris. Praesent molestie convallis tortor, mollis tristique arcu fermentum interdum. Phasellus eleifend fringilla ullamcorper. Donec tristique, dui at blandit pretium, ex sem egestas dolor, vel fermentum magna dui at ligula. Nam erat neque, maximus et nibh eu, vehicula vestibulum lectus. Aenean non nunc sed enim pellentesque interdum. In venenatis elit nulla. Donec vulputate purus vel tortor feugiat, eu finibus odio tempus. Aliquam feugiat lacus at dolor aliquet congue. Curabitur posuere lacinia tortor non facilisis. Praesent varius, mi ut suscipit molestie, tellus risus cursus libero, in tincidunt nisl dolor ac mauris. Praesent molestie convallis tortor, mollis tristique arcu fermentum interdum. Phasellus eleifend fringilla ullamcorper. Donec tristique, dui at blandit pretium, ex sem egestas dolor, vel fermentum magna dui at ligula. Nam erat neque, maximus et nibh eu, vehicula vestibulum lectus. Aenean non nunc sed enim pellentesque interdum. In venenatis elit nulla. Donec vulputate purus vel tortor feugiat, eu finibus odio tempus. Aliquam feugiat lacus at dolor aliquet congue.'
       />
     )
   }
