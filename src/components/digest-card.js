909c928b80f5f80682dd77877dc8d2bcbea4d2cf
Graham Grochowski <ggrochow@gmail.com>
2015-12-27 12:52:13 -0800

    added links to digests and sections to link to readviews

--- src/components/digest-card.js
+++ src/components/digest-card.js
@@ -2,10 +2,15 @@ import React from 'react'
 import { Card, CardTitle } from 'react-mdl'
 
 export default class DigestCard extends React.Component {
+  readFull = () => {
+    this.props.readFull(this.props.id)
+  }
+
   render() {
     return (
       <Card className='mdl-card-dash mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-shadow--2dp'
-            style={{ background: `url('${this.props.imageUrl}') center / cover no-repeat #46B6AC`}}>
+            style={{ background: `url('${this.props.imageUrl}') center / cover no-repeat #46B6AC`}}
+            onClick={this.readFull}>
         <CardTitle>
           <h2 className='mdl-card__title-text'>{this.props.publishedAt}</h2>
         </CardTitle>
--- src/components/digest-list.js
+++ src/components/digest-list.js
@@ -1,26 +1,39 @@
 import React from 'react'
 import DigestCard from './digest-card'
+import { connect } from 'react-redux'
+import { pushPath } from 'redux-simple-router'
 
-export default class DigestList extends React.Component {
+class DigestList extends React.Component {
+  readFullDigest = (id) => {
+    this.props.dispatch(pushPath(`/digests/${id}`))
+  }
 
   render() {
     return (
     <div className="mdl-grid">
       <DigestCard 
+        id='1'
         publishedAt='19 December 2015'
         imageUrl='http://tfirdaus.github.io/mdl/images/laptop.jpg'
+        readFull={this.readFullDigest}
       />
 
-      <DigestCard 
+      <DigestCard
+        id='2'
         publishedAt='25 December 2015'
         imageUrl='http://i.imgur.com/gO3vYga.jpg'
+        readFull={this.readFullDigest}
       />
 
-      <DigestCard 
+      <DigestCard
+        id='3'
         publishedAt='01 January 2016'
         imageUrl='http://i.imgur.com/NPsCCKZ.jpg'
+        readFull={this.readFullDigest}
       />
     </div>
     )
   }
 }
+
+export default connect()( DigestList )
