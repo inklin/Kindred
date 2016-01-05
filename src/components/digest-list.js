e588401ec3f5d0c8b1bfdd79cf834e7155504145
Graham Grochowski <ggrochow@gmail.com>
2016-01-04 19:15:25 -0800

    moved getting of imageurl to another function

--- src/components/digest-list.js
+++ src/components/digest-list.js
@@ -11,32 +11,35 @@ class DigestList extends React.Component {
     this.props.dispatch(pushPath(`/digests/${id}`))
   }
 
-  render() {
+  getImageUrl = (id) => {
     function getRandomArrayElement(arr){
       return arr[Math.floor(Math.random() * arr.length)]
     }
 
+    let imageUrl
+    let digest = this.props.digests.get(id)
+    // gets random image URL to display in digest page, clean up this messyness
+    if ( digest.imageUrl === undefined ){
+      let randUpdate = this.props.updates.get(getRandomArrayElement(digest.updates))
+      let randSection = this.props.sections.get(getRandomArrayElement(randUpdate.sections))
+      imageUrl = randSection.imageUrl
+
+      this.props.dispatch( saveImageUrl(digest.id, imageUrl) )
+    } else { 
+      imageUrl = digest.imageUrl
+    }
+    return imageUrl
+  }
+
+  render() {
     let digests = []
 
-    this.props.digests.forEach ( (digest) => {
-      let imageUrl
-
-      // gets random image URL to display in digest page, clean up this messyness
-      if ( digest.imageUrl === undefined ){
-        let randUpdate = this.props.updates.get(getRandomArrayElement(digest.updates))
-        let randSection = this.props.sections.get(getRandomArrayElement(randUpdate.sections))
-        imageUrl = randSection.imageUrl
-
-        this.props.dispatch( saveImageUrl(digest.id, imageUrl) )
-      } else { 
-        imageUrl = digest.imageUrl
-      }
-      
+    this.props.digests.forEach ( (digest) => {      
 
       digests.push(<DigestCard 
         id={digest.id}
         key={digest.id}
-        imageUrl={imageUrl}
+        imageUrl={this.getImageUrl(digest.id)}
         publishedAt={digest.sentAt}
         readFull={this.readFullDigest}
         />)
