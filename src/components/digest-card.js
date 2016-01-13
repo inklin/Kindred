e2630aeef70ae018a77f00053c70fa5d60ad4e77
Emilie Hester <emiliehester@gmail.com>
2016-01-12 19:21:32 -0800

    Upgraded styling of update cards

--- src/components/digest-card.js
+++ src/components/digest-card.js
@@ -8,13 +8,23 @@ export default class DigestCard extends React.Component {
 
   render() {
     let publishedAt = new Date(this.props.publishedAt)
-    let DateString = publishedAt.toDateString()
+    let DateArray = publishedAt.toDateString().split(" ")
+    let months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+    let Month = months[publishedAt.getMonth()] || "Undefined"
+    let Day = DateArray[2]  || "Undefined"
+    let Year = DateArray[3]  || "Undefined"
     return (
       <Card className='mdl-card-dash mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-shadow--2dp'
             style={{ background: `url('${this.props.imageUrl}') center / cover no-repeat #657153`}}
             onClick={this.readFull}>
         <CardTitle>
-          <h2 className='mdl-card__title-text'>{DateString}</h2>
+          <div className='digest-card-box'>
+            <div className='digest-card-date-box'>
+              <p className='mdl-card__title-text digest-card-date-day'>{Day}</p>
+              <p className='mdl-card__title-text digest-card-date-month'>{Month}</p>
+              <p className='mdl-card__title-text digest-card-date-year'>{Year}</p>
+            </div>
+          </div>
         </CardTitle>
       </Card>
     )
